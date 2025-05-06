4116d47d49727db36e2002c9b6ac5435189e6e98
Tisho <tihomirpetrov79@gmail.com>
2025-05-06 21:58:16 +0300

    changed routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import {Routes} from "@angular/router";
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+];
